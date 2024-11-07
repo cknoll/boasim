0ad24503c93b02090efcf3e4b1b3c9564bd44ab9
Carsten Knoll <carsten.knoll@posteo.de>
2024-11-07 16:06:00 +0100

    cli.py: add option --version

--- src/boasim/cli.py
+++ src/boasim/cli.py
@@ -1,10 +1,11 @@
 """
-Command line interface for autobrowser package
+Command line interface
 """
 
 import argparse
 from ipydex import IPS, activate_ips_on_exception
 from . import core
+from . import __version__
 
 activate_ips_on_exception()
 
@@ -12,9 +13,14 @@ activate_ips_on_exception()
 def main():
 
     parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     "cmd", help=f"main command (not yet defined)"
+    # )
     parser.add_argument(
-        "cmd", help=f"main command (not yet defined)"
+        "--version", help=f"print version and exit", action="store_true"
     )
 
     args = parser.parse_args()
 
+    if args.version:
+        print(__version__)
