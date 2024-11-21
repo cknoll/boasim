17b6139a46c44563e839382d925187c36e6cd474
Carsten Knoll <carsten.knoll@posteo.de>
2024-11-21 15:17:35 +0100

    __init__.py: improve reloading capability (simplify co-development of notebook and package)

--- src/boasim/__init__.py
+++ src/boasim/__init__.py
@@ -1,12 +1,32 @@
 from .release import __version__
+import sys
 
 # the package is imported during installation (to obtain the version)
 # however installation happens in an isolated build environment
 # where no dependencies are installed.
+# the following try-except block ensures that the installation works
+# (where dependencies are not available)
 
 try:
+
     from . import core
     from . import blocks
+
+    # This mechanism serves to simplify the co-development of the package
+    # and a jupyter notebook: one reload-statement is then sufficient
+    # to also reload submodules
+
+    attr_name = f"_package_{__name__}_is_imported"
+
+    if getattr(sys, attr_name, False):
+        # print(f"reloading package {__name__}")
+        import importlib as il
+        il.reload(core)
+        il.reload(blocks)
+    else:
+        # print(f"importing package {__name__}")
+        setattr(sys, attr_name, True)
+
     from .core import *
     from .blocks import *
 except ImportError:
